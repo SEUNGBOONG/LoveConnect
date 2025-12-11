eb0753f5f5c08fefb6fb4e3cb32a9a6de2f6df16
승엽 <tmdduqflfl@naver.com>
2025-12-11 13:56:53 +0900

    Feat: BreakUp 이별사유

---
+++ demo/src/main/java/com/example/demo/breakup/domain/BreakupReason.java
@@ -0,0 +1,39 @@
+package com.example.demo.breakup.domain;
+
+import com.example.demo.login.member.domain.member.Member;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BreakupReason {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private List<BreakupReasonType> reasons;
+
+    private String etcReason;
+}
