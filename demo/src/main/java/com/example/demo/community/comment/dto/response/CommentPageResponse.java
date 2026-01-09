679766377ab98040c418874374fba83dbcd5ce07
승엽 <tmdduqflfl@naver.com>
2026-01-09 13:18:18 +0900

    Refactoring: Comment

--- demo/src/main/java/com/example/demo/community/comment/dto/response/CommentPageResponse.java
+++ demo/src/main/java/com/example/demo/community/comment/dto/response/CommentPageResponse.java
@@ -4,5 +4,13 @@ import java.util.List;
 
 public record CommentPageResponse(
         List<CommentResponse> comments,
-        long totalCount
-) {}
+
+        long totalCount,        // 전체 댓글 수 (부모+자식)
+        int totalPages,
+        int pageNumber,
+        int pageSize,
+        boolean first,
+        boolean last,
+        int numberOfElements
+) {
+}
