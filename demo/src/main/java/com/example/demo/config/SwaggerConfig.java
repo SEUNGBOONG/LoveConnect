e1bc3e1702d166a8773a4eaef15342ef7af8cccc
승엽 <tmdduqflfl@naver.com>
2026-01-12 17:11:59 +0900

    Refactoring: Comment

--- demo/src/main/java/com/example/demo/config/SwaggerConfig.java
+++ demo/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -11,6 +14,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .servers(List.of( // ✅ 서버 URL 명시 (https)
+                        new Server().url("https://api.lovereconnect.co.kr")
+                ))
                 .info(new Info()
                         .title("Demo API 명세서")
                         .description("Spring Boot + Swagger API 문서")
