00662e0f2bb90fd0da94925fe087eb34b619ebfe
승엽 <tmdduqflfl@naver.com>
2026-01-26 16:14:47 +0900

    Feat: tiktok

--- demo/src/main/java/com/example/demo/login/global/annotation/Member.java
+++ demo/src/main/java/com/example/demo/login/global/annotation/Member.java
@@ -1,5 +1,7 @@
 package com.example.demo.login.global.annotation;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -9,6 +11,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Parameter(hidden = true)
 public @interface Member {
     boolean required() default true;
 }
