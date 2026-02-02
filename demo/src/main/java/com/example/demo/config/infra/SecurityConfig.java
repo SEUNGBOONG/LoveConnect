f30fae5cb6a30d25967ee28ca393d476eb7f0828
승엽 <tmdduqflfl@naver.com>
2026-02-02 17:51:07 +0900

    Fix: Cookie

--- demo/src/main/java/com/example/demo/config/infra/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/infra/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.example.demo.login.member.infrastructure.auth.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -44,7 +43,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**"
                         ).permitAll()
                         .anyRequest().authenticated()
-                )
+                );
 
         return http.build();
     }
