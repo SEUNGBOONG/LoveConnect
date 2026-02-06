400b5dd86da34034c3f7d593828d402e114b454e
승엽 <tmdduqflfl@naver.com>
2026-02-06 17:15:18 +0900

    Refactoring: TOSS LOGIN

--- demo/src/main/java/com/example/demo/config/toss/TossHttpConfig.java
+++ demo/src/main/java/com/example/demo/config/toss/TossHttpConfig.java
@@ -29,33 +29,27 @@ public class TossHttpConfig {
 
     @Bean(name = "tossRestTemplate")
     public RestTemplate tossRestTemplate() throws Exception {
-        // 1. KeyStore 로드 (.p12 인증서)
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
         try (InputStream is = ResourceUtils.getURL(certPath).openStream()) {
             keyStore.load(is, certPassword.toCharArray());
         }
 
-        // 2. SSLContext 생성 (인증서 포함)
         SSLContext sslContext = SSLContextBuilder.create()
                 .loadKeyMaterial(keyStore, certPassword.toCharArray())
                 .build();
 
-        // 3. SSLConnectionSocketFactory 설정 (HttpClient 5 방식)
         SSLConnectionSocketFactory sslSocketFactory = SSLConnectionSocketFactoryBuilder.create()
                 .setSslContext(sslContext)
                 .build();
 
-        // 4. HttpClient 생성 (Spring Boot 3 / HttpClient 5 환경)
         CloseableHttpClient httpClient = HttpClients.custom()
                 .setConnectionManager(PoolingHttpClientConnectionManagerBuilder.create()
                         .setSSLSocketFactory(sslSocketFactory)
                         .build())
                 .build();
 
-        // 5. RestTemplate에 HttpClient 5 연결
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        factory.setConnectTimeout((int) Duration.ofSeconds(5).toMillis()); // 연결 타임아웃
-        // Spring Boot 3의 경우 setReadTimeout 대신 다른 방식이나 기본 설정 사용 가능
+        factory.setConnectTimeout((int) Duration.ofSeconds(5).toMillis());
 
         return new RestTemplate(factory);
     }
