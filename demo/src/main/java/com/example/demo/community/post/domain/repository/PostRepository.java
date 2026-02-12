f49eab1f5142d49b93d318fe8f5ae8c75b71294f
승엽 <tmdduqflfl@naver.com>
2026-02-12 20:52:39 +0900

    Refactoring: TOSS LOGIN

--- demo/src/main/java/com/example/demo/community/post/domain/repository/PostRepository.java
+++ demo/src/main/java/com/example/demo/community/post/domain/repository/PostRepository.java
@@ -4,8 +4,7 @@ import com.example.demo.community.post.domain.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostQueryRepository {
     @EntityGraph(attributePaths = "writer")
